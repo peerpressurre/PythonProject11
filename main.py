7636fadbabe85e29590cb79bb8fe409af1d6b0e5
“Your <yaroslavdisiak@gmail.com>
2022-10-29 09:58:56 +0300

    Update:feature/lab_ex1

---
+++ main.py
@@ -0,0 +1,11 @@
+def rec(a, b):
+    try:
+        for i in range(a):
+            for j in range(b):
+                print('*', end=' ')
+            print()
+        print()
+    except Exception as ex:
+        print(f'Error: {ex}')
+
+rec(3,4)
