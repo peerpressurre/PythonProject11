ef559816ffa40f34ffd6a1cf37d642cb98309179
“Your <yaroslavdisiak@gmail.com>
2022-10-29 10:44:15 +0300

    Update:feature/lab_ex4

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 def rec(b):
     try:
         counter = 0
-        for i in range(1, 10):
-            if b / i == 0 or b / b == 0:
+        for i in range(1, b+1):
+            if b % i == 0:
                 counter += 1
-                print(counter)
+
         if counter == 2:
             print(f'{b} є простим числом')
         else:
             print(f'{b} не є простим числом')
-        print(counter)
+
     except Exception as ex:
         print(f'Error information: {ex}')
 
-rec(3)
+
+number = int(input("number->"))
+rec(number)
